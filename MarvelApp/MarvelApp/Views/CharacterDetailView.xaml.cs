1e586726df2114b2570d2b8d2b488e3ae28ee890
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-08-03 16:11:40 -0300

    BottomSheet configurado e agora funcionando na página CharacterDetailsView.

--- MarvelApp/MarvelApp/Views/CharacterDetailView.xaml.cs
+++ MarvelApp/MarvelApp/Views/CharacterDetailView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,32 @@ using Xamarin.Forms.Xaml;
 
 namespace MarvelApp.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(true)]
     public partial class CharacterDetailView : ContentPage
     {
         public CharacterDetailView()
         {
             InitializeComponent();
+        }
+        bool IsBottomSheetVisible = false;
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!IsBottomSheetVisible)
+            {
+                BottomSheetId.IsVisible = IsBottomSheetVisible = true;
+                await Task.Delay(100);
+                await BottomSheetId.TranslateTo(0, 400, 400, Easing.SinIn);
+
+            }
+            else
+            {
+
+                await BottomSheetId.TranslateTo(0, 600, 550, Easing.SinOut);
+                await Task.Delay(100);
+                BottomSheetId.IsVisible = IsBottomSheetVisible = false;
+
+            }
+
         }
     }
 }
