7382359b204d45ee35907730eadfb05ba013ea99
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-08-03 23:37:28 -0300

    Alguns testes de interface foram adicionados, mas há um problema com o Android_Home.

---
+++ MarvelApp.UITest/Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace MarvelApp.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+        AppQuery CollectionView;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            // app.WaitForNoElement("heroescollectionview","", TimeSpan.FromSeconds(20));
+
+            app.EnterText("SearchView", "Bomb");
+            app.PressEnter();
+            app.Screenshot("Procurar Bomb");
+             Task.Delay(250);
+
+
+
+        }
+
+        public void IndexCollectionView(int Position)
+        {
+            app.ScrollDownTo(Position.ToString(), "", ScrollStrategy.Gesture, 0.67, 500, true, TimeSpan.FromSeconds(10000));
+            //app.ScrollTo(heroescollectionview);
+        }
+        [Test]
+
+        public async Task NextView()
+        {
+            app.Tap("ImagePath");
+            await Task.Delay(3000);
+            app.Screenshot("Próxima View");
+
+            app.Back();
+        }
+        [Test]
+        public void BackView()
+        {
+            app.Back();
+            app.Screenshot("View Anterior");
+        }
+        [Test]
+        public void ScreenLandscape()
+        {
+            app.SetOrientationLandscape();
+            app.Screenshot("Tela na Horizontal");
+        }
+        [Test]
+        public void ScreenPortrait()
+        {
+            app.SetOrientationPortrait();
+            app.Screenshot("Tela na Vertical");
+        }
+    }
+}
