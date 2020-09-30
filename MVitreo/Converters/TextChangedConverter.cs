460ad14f3f1cf2f61b4804fd2c91ae5eb308aeb9
Paulo Junior <paulo.avelinojunior@outlook.com>
2020-09-30 05:54:50 -0300

    SearchBar Converter

---
+++ MVitreo/Converters/TextChangedConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MVitreo.Converters
+{
+    public class TextChangedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is TextChangedEventArgs textChangedEventArgs))
+                throw new ArgumentException("Expected value to be of type ItemTappedEventArgs", nameof(value));
+
+            return textChangedEventArgs.NewTextValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
